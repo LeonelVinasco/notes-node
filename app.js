d05f00b78e06ceef9d6c57ae4ec74b94b6843032
leo <leonelvinasco@gmail.com>
2018-08-02 11:52:22 -0500

    empezando el curso de NodeJS

---
+++ app.js
@@ -0,0 +1,19 @@
+console.log('Empezando');
+const fs=require('fs');
+const os=require('os');
+const notes=require('./notes.js');
+
+console.log('Result:', notes.add(9,-2));
+//var res= notes.addNote();
+
+// var user= os.Llamar funciones:userInfo();
+// console.log(user);
+//
+// fs.appendFile('felicitaciones.txt',` Hola ${user.username} ¿cómo estás?
+//
+//                ya, tú tienes: ${notes.age}`, function (err){
+//   if (err){
+//     console.log('No se puede abrir el archivo')
+//   }
+// });
+// console.log('terminando');
---
+++ notes.js
@@ -0,0 +1,10 @@
+console.log('starting notes app');
+module.exports.age=25;
+module.exports.addNote=() =>{
+  console.log('addNote');
+  return 'New Note'
+
+}
+module.exports.add=(a,b)=>{
+  return a+b;
+};
